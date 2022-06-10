2918bc89cb351f09b86c1b4407044e071f8c0241
Kuellar <icuellartapia@gmail.com>
2022-06-09 20:53:41 -0400

    FIX mobile version

--- actions.js
+++ actions.js
@@ -7,16 +7,20 @@ document
  */
 const change_menu = () => {
     const menu = document.getElementById("menu-section");
-    const menuIcon = document.getElementById("alternator-icon");
+    const alternatorButton = document.getElementById("alternator");
+    const alternatorIcon = document.getElementById("alternator-icon");
     if (menu.classList.contains("menu-section-init")) {
         menu.classList.remove("menu-section-init");
         menu.classList.add("menu-section-close");
-        menuIcon.classList.add("alternator-left");
+        alternatorIcon.classList.add("alternator-icon-left");
+        alternatorButton.classList.add("alternator-left");
     } else {
         menu.classList.toggle("menu-section-open");
         menu.classList.toggle("menu-section-close");
-        menuIcon.classList.toggle("alternator-right");
-        menuIcon.classList.toggle("alternator-left");
+        alternatorIcon.classList.toggle("alternator-icon-right");
+        alternatorIcon.classList.toggle("alternator-icon-left");
+        alternatorButton.classList.toggle("alternator-right");
+        alternatorButton.classList.toggle("alternator-left");
     }
 };
 document.getElementById("alternator").addEventListener("click", change_menu);
